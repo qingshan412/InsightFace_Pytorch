157af1776de3cdbfe995811586d7a356eef5c5d4
qingshan412 <qingshan412@hotmail.com>
2019-11-10 18:48:49 -0500

    data_processing

---
+++ data/order_num.py
@@ -0,0 +1,9 @@
+import os
+
+path = "facebank" + os.sep() + "noonan"
+dirs = os.listdir(path)
+
+i = 1
+for file_name in dirs:
+   os.rename(path + os.sep() + file_name, path + os.sep() + str(i) + ".jpg")
+   i += 1
