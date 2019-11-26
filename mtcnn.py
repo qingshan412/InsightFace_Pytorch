b48a4a953820c7456da73b83bc52ffeb2e98535d
qingshan412 <qingshan412@hotmail.com>
2019-11-25 18:05:58 -0800

    test

--- mtcnn.py
+++ mtcnn.py
@@ -88,7 +88,7 @@ class MTCNN():
             # collect boxes (and offsets, and scores) from different scales
             bounding_boxes = [i for i in bounding_boxes if i is not None]
             print('\n\n\n')
-            print(bounding_boxes.shape)
+            print(len(bounding_boxes.shape))
             print('\n\n\n')
             bounding_boxes = np.vstack(bounding_boxes)
 
