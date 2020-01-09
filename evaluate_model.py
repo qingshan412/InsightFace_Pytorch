d3056b11311ef400822a40c73cdd3cfb7e6d4c85
qingshan412 <qingshan412@hotmail.com>
2020-01-09 18:51:55 -0500

    Update evaluate_model.py

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,19 +1,19 @@
-import pdb
-from config import get_config
-import argparse
-from Learner import face_learner
-from data.data_pipe import get_val_pair
-from torchvision import transforms as trans
-from pathlib import Path
-from data.data_pipe import load_noonan_val_pair
+# import pdb
+# from config import get_config
+# import argparse
+# from Learner import face_learner
+# from data.data_pipe import get_val_pair
+# from torchvision import transforms as trans
+# from pathlib import Path
+# from data.data_pipe import load_noonan_val_pair
 
-conf = get_config(training=False)
-learner = face_learner(conf, inference=True)
-learner.load_state(conf, 'mobilefacenet.pth', True, True)
-fp, issame = load_noonan_val_pair('data/facebank/noonan+normal/raw_112', Path('data/facebank/noonan+normal/val_112'), conf.test_transform)
-accuracy, best_threshold, roc_curve_tensor = learner.evaluate(conf, fp, issame, nrof_folds=10, tta=True)
-print('fp - accuray:{}, threshold:{}'.format(accuracy, best_threshold))
-trans.ToPILImage()(roc_curve_tensor)
+# conf = get_config(training=False)
+# learner = face_learner(conf, inference=True)
+# learner.load_state(conf, 'mobilefacenet.pth', True, True)
+# fp, issame = load_noonan_val_pair('data/facebank/noonan+normal/raw_112', Path('data/facebank/noonan+normal/val_112'), conf.test_transform)
+# accuracy, best_threshold, roc_curve_tensor = learner.evaluate(conf, fp, issame, nrof_folds=10, tta=True)
+# print('fp - accuray:{}, threshold:{}'.format(accuracy, best_threshold))
+# trans.ToPILImage()(roc_curve_tensor)
 
 
 from data.data_pipe import img2lmk
