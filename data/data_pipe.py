1880e16a50a0c639b690a0c65c0e41e434d00800
qingshan412 <qingshan412@hotmail.com>
2020-02-04 13:21:50 -0500

    Update data_pipe.py

--- data/data_pipe.py
+++ data/data_pipe.py
@@ -169,9 +169,9 @@ def img2lmk(img_path, lmk_path, in_place=False, predictor_path='data/lmk_predict
             lmk_img = Image.open(f)
             plt.clf()
             plt.imshow(lmk_img)
-            plt.scatter([p[0] for p in points], [p[1] for p in points], marker='o', color='r', linewidths=5)
+            plt.scatter([p[0] for p in points], [p[1] for p in points], marker='o', color='r', linewidths=2)
             currentAxisA = plt.gca()
-            rect = patches.Rectangle((rec[3], rec[0]), rec[2] - rec[0], rec[1] - rec[3], 
+            rect = patches.Rectangle((rec[3], rec[0]), (rec[2] - rec[0]), (rec[1] - rec[3]), 
                                      linewidth=5, edgecolor='g', facecolor='none')
             currentAxisA.add_patch(rect)
             currentAxisA.axes.get_xaxis().set_visible(False)
