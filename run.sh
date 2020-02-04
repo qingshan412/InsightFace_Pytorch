c54ad2271841851df12e0a8655e9f78ca08ba42d
qingshan412 <qingshan412@hotmail.com>
2020-02-03 23:39:25 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 #$ -M jliu16@nd.edu	 # Email address for job notification
 #$ -m abe		 # Send mail when job begins, ends and aborts
-#$ -pe smp 24	 # Specify parallel environment and legal core size
+#$ -pe smp 4	 # Specify parallel environment and legal core size
 #$ -q long		 # Specify queue
 #$ -N verify_test	         # Specify job name
 
