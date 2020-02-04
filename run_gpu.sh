71b5f62975da24a6ded4463480937f57f3cd3463
qingshan412 <qingshan412@hotmail.com>
2020-02-04 00:03:28 -0500

    Update run_gpu.sh

--- run_gpu.sh
+++ run_gpu.sh
@@ -7,7 +7,7 @@
 #$ -l gpu_card=1
 #$ -N verify_test_gpu         # Specify job name
 
-module load python
+module load pytorch
 
 python face_verify_noonan_fold_gpu.py -g $CUDA_VISIBLE_DEVICES > data/facebank/default_2_gpu
 python face_verify_noonan_fold_gpu.py -tta -g $CUDA_VISIBLE_DEVICES > data/facebank/tta_1_gpu
