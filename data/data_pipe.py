bcc481e9499a8a11f8b83cac590e9428b3dd13fa
qingshan412 <qingshan412@hotmail.com>
2020-02-04 13:56:31 -0500

    Update data_pipe.py

--- data/data_pipe.py
+++ data/data_pipe.py
@@ -161,6 +161,8 @@ def img2lmk(img_path, lmk_path, in_place=False, predictor_path='data/lmk_predict
             print("Number of faces detected: {}".format(len(dets)))
             continue
         rec = [dets[0].left(), dets[0].top(), dets[0].right(), dets[0].bottom()]
+        print(rec)
+        exit(0)
         
         shape = predictor(img, dets[0])
         points = [(p.x, p.y) for p in shape.parts()]
@@ -169,7 +171,7 @@ def img2lmk(img_path, lmk_path, in_place=False, predictor_path='data/lmk_predict
             lmk_img = Image.open(f)
             plt.clf()
             plt.imshow(lmk_img)
-            plt.scatter([p[0] for p in points], [p[1] for p in points], marker='o', color='r', linewidths=2)
+            plt.scatter([p[0] for p in points], [p[1] for p in points], marker='o', color='r', linewidths=1)
             currentAxisA = plt.gca()
             rect = patches.Rectangle((rec[3], rec[0]), (rec[2] - rec[0]), (rec[1] - rec[3]), 
                                      linewidth=5, edgecolor='g', facecolor='none')
