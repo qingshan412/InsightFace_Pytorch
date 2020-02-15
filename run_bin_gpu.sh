2233a5cb34c648ac45cd409126910fc798abe5c2
qingshan412 <qingshan412@hotmail.com>
2020-02-15 12:47:20 -0500

    bin test

---
+++ run_bin_gpu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#$ -M jliu16@nd.edu      # Email address for job notification
+#$ -m abe                # Send mail when job begins, ends and aborts
+#$ -pe smp 16            # Specify parallel environment and legal core size
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -N bin_test_gpu         # Specify job name
+
+module load pytorch
+
+python bin_roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES > data/facebank/bin_0
