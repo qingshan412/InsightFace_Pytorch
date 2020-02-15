59bc656f04fb0b7ed6b97483ddad6f185f0ee45e
qingshan412 <qingshan412@hotmail.com>
2020-02-15 11:31:03 -0500

    Update run_gpu.sh

--- run_gpu.sh
+++ run_gpu.sh
@@ -9,7 +9,9 @@
 
 module load pytorch
 
-python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES > data/facebank/roc_0
+python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -s > data/facebank/roc_s_0
+python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -tta > data/facebank/roc_tta_0
+python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -s -tta > data/facebank/roc_s_tta_0
 # python face_verify_noonan_fold_gpu.py -g $CUDA_VISIBLE_DEVICES > data/facebank/default_2_gpu
 # python face_verify_noonan_fold_gpu.py -tta -g $CUDA_VISIBLE_DEVICES > data/facebank/tta_1_gpu
 # python face_verify_noonan_fold_gpu.py -s -g $CUDA_VISIBLE_DEVICES > data/facebank/shuffle_1_gpu
