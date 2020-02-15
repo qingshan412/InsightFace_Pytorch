dbdf9249d0f0ab78eb94f58db9233c14537727a1
qingshan412 <qingshan412@hotmail.com>
2020-02-15 11:28:52 -0500

    Update Learner.py

--- Learner.py
+++ Learner.py
@@ -252,3 +252,28 @@ class face_learner(object):
         minimum, min_idx = torch.min(dist, dim=1)
         min_idx[minimum > self.threshold] = -1 # if no match, set idx to -1
         return min_idx, minimum 
+
+    def binfer(self, conf, faces, target_embs, tta=False):
+        '''
+        infer as binary classification
+        faces : list of PIL Image
+        target_embs : [n, 512] computed embeddings of faces in facebank
+        names : recorded names of faces in facebank
+        tta : test time augmentation (hfilp, that's all)
+        '''
+        embs = []
+        for img in faces:
+            if tta:
+                mirror = trans.functional.hflip(img)
+                emb = self.model(conf.test_transform(img).to(conf.device).unsqueeze(0))
+                emb_mirror = self.model(conf.test_transform(mirror).to(conf.device).unsqueeze(0))
+                embs.append(l2_norm(emb + emb_mirror))
+            else:                        
+                embs.append(self.model(conf.test_transform(img).to(conf.device).unsqueeze(0)))
+        source_embs = torch.cat(embs)
+        
+        diff = source_embs.unsqueeze(-1) - target_embs.transpose(1,0).unsqueeze(0)
+        dist = torch.sum(torch.pow(diff, 2), dim=1)
+        minimum, min_idx = torch.min(dist, dim=1)
+        min_idx[minimum > self.threshold] = -1 # if no match, set idx to -1
+        return min_idx, minimum 
