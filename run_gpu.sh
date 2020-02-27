1edc17d0735d88fecd6b020d39f3c5252c203885
qingshan412 <qingshan412@hotmail.com>
2020-02-27 17:06:48 -0500

    Update run_gpu.sh

--- run_gpu.sh
+++ run_gpu.sh
@@ -9,10 +9,10 @@
 
 module load pytorch
 
-python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES > data/facebank/roc_resize_112
-# python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -s > data/facebank/roc_s_resize_112
-# python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -tta > data/facebank/roc_tta_resize_112
-# python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -s -tta > data/facebank/roc_s_tta_resize_112
+python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES > data/facebank/roc_raw_112
+python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -s > data/facebank/roc_s_raw_112
+python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -tta > data/facebank/roc_tta_raw_112
+python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -s -tta > data/facebank/roc_s_tta_raw_112
 # python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -s > data/facebank/roc_s_0
 # python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -tta > data/facebank/roc_tta_0
 # python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -s -tta > data/facebank/roc_s_tta_0
