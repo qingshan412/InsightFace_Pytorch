1008b4d7a7883939796366a4e2e50c7f8dd24573
qingshan412 <qingshan412@hotmail.com>
2020-03-14 12:17:07 -0400

    Update run_gpu.sh

--- run_gpu.sh
+++ run_gpu.sh
@@ -9,6 +9,22 @@
 
 module load pytorch
 
+for Epoch in 10 15
+do
+    for Type in 'raw' 'resize' 'raw_resize'
+    do
+        for Model in 'fr_adult_pix2pix_transfer_b6_25_1layer_fe' 'fr_adult_pix2pix_transfer_b6_25_2layer' 'fr_adult_pix2pix_transfer_b6_25_1layer_pool5_DG'
+        do
+            python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -a fake_${Type}_${Epoch}_${Model} \
+            > data/facebank/roc_${Type}_${Epoch}_${Model}
+            python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -s -a fake_${Type}_${Epoch}_${Model} \
+            > data/facebank/roc_${Type}_${Epoch}_${Model}_s
+            python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -tta -a fake_${Type}_${Epoch}_${Model} \
+            > data/facebank/roc_${Type}_${Epoch}_${Model}_tta
+        done
+    done
+done
+
 python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -a "fake_resize" > data/facebank/roc_raw_112_a_resize
 python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -s -a "fake_resize" > data/facebank/roc_raw_112_s_a_resize
 python roc_acc_fold.py -g $CUDA_VISIBLE_DEVICES -tta -a "fake_resize" > data/facebank/roc_raw_112_tta_a_resize
