b5744853620c6a3b5eeff97e09ffb3b1ebc48654
qingshan412 <qingshan412@hotmail.com>
2020-03-14 13:46:10 -0400

    Update mtcnn.py

--- mtcnn.py
+++ mtcnn.py
@@ -88,7 +88,6 @@ class MTCNN():
             # collect boxes (and offsets, and scores) from different scales
             bounding_boxes = [i for i in bounding_boxes if i is not None]
             bounding_boxes = np.vstack(bounding_boxes)
-            print('after vstack:', bounding_boxes.shape)
             keep = nms(bounding_boxes[:, 0:5], nms_thresholds[0])
             bounding_boxes = bounding_boxes[keep]
 
@@ -147,5 +146,7 @@ class MTCNN():
             keep = nms(bounding_boxes, nms_thresholds[2], mode='min')
             bounding_boxes = bounding_boxes[keep]
             landmarks = landmarks[keep]
+            print('before return:', bounding_boxes.shape)
+
 
         return bounding_boxes, landmarks
