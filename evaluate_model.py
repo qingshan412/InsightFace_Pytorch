24c4be062639135a852e8f949728811a3976b724
qingshan412 <qingshan412@hotmail.com>
2020-03-16 18:51:37 -0400

    Update evaluate_model.py

--- evaluate_model.py
+++ evaluate_model.py
@@ -55,12 +55,13 @@
 from data.data_pipe import get_vague_faces
 
 records = ['raw_fr_lag_aug_pix2pix_transfer_b6_100_2layer', 'fr_lag_aug_pix2pix_transfer_b6_100_2layer',
-            'fr_lag_pix2pix_transfer_b6_100_2layer']
-for rec in records:
-    print('processing', rec)
-    source_path = '../pytorch-CycleGAN-and-pix2pix/results/' + rec + '/test_latest/images'
-    save_path = 'data/facebank/noonan+normal/fake_' + rec
-    get_vague_faces(source_path, save_path)
+            'fr_lag_pix2pix_transfer_b6_100_2layer', 'fr_lag_aug_pix2pix_transfer_b6_500_2layer']
+# for rec in records:
+rec = records[-1]
+print('processing', rec)
+source_path = '../pytorch-CycleGAN-and-pix2pix/results/' + rec + '/test_latest/images'
+save_path = 'data/facebank/noonan+normal/fake_' + rec
+get_vague_faces(source_path, save_path)
 
 # source_path = '../pytorch-CycleGAN-and-pix2pix/results/fr_aug_pix2pix_transfer_b6_100_2layer/test_latest/images'
 # save_path = 'data/facebank/noonan+normal/fake_fr_aug_pix2pix_transfer_b6_100_2layer'
