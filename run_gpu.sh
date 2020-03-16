92b7feafc9d6b1ac72b8f564c89faeeb920b07f1
qingshan412 <qingshan412@hotmail.com>
2020-03-16 18:35:26 -0400

    Update run_gpu.sh

--- run_gpu.sh
+++ run_gpu.sh
@@ -11,6 +11,7 @@ module load pytorch
 
 for Model in raw_fr_lag_aug_pix2pix_transfer_b6_100_2layer fr_lag_aug_pix2pix_transfer_b6_100_2layer fr_lag_pix2pix_transfer_b6_100_2layer
 do
+    echo "processing ${Model}..."
     python roc_acc_fold.py -g 0 -a fake_${Model} > data/facebank/roc_${Model}
     python roc_acc_fold.py -g 0 -s -a fake_${Model} > data/facebank/roc_${Model}_s
     python roc_acc_fold.py -g 0 -tta -a fake_${Model} > data/facebank/roc_${Model}_tta
