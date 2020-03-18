63050f40f9d94558473cc35f659d505e3906a0cc
qingshan412 <qingshan412@hotmail.com>
2020-03-17 22:14:18 -0400

    Update roc_acc_fold.py

--- roc_acc_fold.py
+++ roc_acc_fold.py
@@ -124,8 +124,7 @@ if __name__ == '__main__':
         if args.additional_data_dir:
             full_additional_dir = conf.data_path/'facebank'/'noonan+normal'/args.additional_data_dir
             add_data = glob.glob(str(full_additional_dir) + os.sep + '*.png')
-            print('additional:', args.additional_data_dir)
-            print(len(add_data))
+            print('additional:', args.additional_data_dir, len(add_data))
             for name in names_considered:
                 for img_f in add_data:
                     if name in img_f.strip().split(os.sep)[-1]:
