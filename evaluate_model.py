2868555c0612bc6ff6c03791c4afb4934d0e5867
qingshan412 <qingshan412@hotmail.com>
2020-03-17 21:58:32 -0400

    Update evaluate_model.py

--- evaluate_model.py
+++ evaluate_model.py
@@ -86,24 +86,24 @@
 ##############################################################################################
 # generate images for data augmentation in gan
 ##############################################################################################
-from data.data_pipe import get_train_dataset_gan, img2lmk
+# from data.data_pipe import get_train_dataset_gan, img2lmk
 
-print('get images from mtcnn...')
-imgs_folder = 'data/facebank/distinct/orig'
-# imgs_folder = 'data/facebank/LAG_y_fine/orig'
-# imgs_folder = 'data/facebank/noonan+normal/raw'
-target_size = 112 + 5
-target_folder = 'data/facebank/distinct/mtcnn_' + str(target_size)
-# target_folder = 'data/facebank/LAG_y_fine/mtcnn_' + str(target_size)
-# target_folder = 'data/facebank/noonan+normal/mtcnn_' + str(target_size)
-get_train_dataset_gan(imgs_folder, target_folder, (target_size, target_size))
+# print('get images from mtcnn...')
+# imgs_folder = 'data/facebank/distinct/orig'
+# # imgs_folder = 'data/facebank/LAG_y_fine/orig'
+# # imgs_folder = 'data/facebank/noonan+normal/raw'
+# target_size = 112 + 5
+# target_folder = 'data/facebank/distinct/mtcnn_' + str(target_size)
+# # target_folder = 'data/facebank/LAG_y_fine/mtcnn_' + str(target_size)
+# # target_folder = 'data/facebank/noonan+normal/mtcnn_' + str(target_size)
+# get_train_dataset_gan(imgs_folder, target_folder, (target_size, target_size))
 
-print('get landmarks...')
-img_path = target_folder
-lmk_path = 'data/facebank/distinct/lmks_mtcnn_' + str(target_size)
-# lmk_path = 'data/facebank/LAG_y_fine/lmks_mtcnn_' + str(target_size)
-# lmk_path = 'data/facebank/noonan+normal/lmks_mtcnn_' + str(target_size)
-img2lmk(img_path, lmk_path, in_place=False)
+# print('get landmarks...')
+# img_path = target_folder
+# lmk_path = 'data/facebank/distinct/lmks_mtcnn_' + str(target_size)
+# # lmk_path = 'data/facebank/LAG_y_fine/lmks_mtcnn_' + str(target_size)
+# # lmk_path = 'data/facebank/noonan+normal/lmks_mtcnn_' + str(target_size)
+# img2lmk(img_path, lmk_path, in_place=False)
 
 ##############################################################################################
 # get_lag_y_data
