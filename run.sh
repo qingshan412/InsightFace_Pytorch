c36f28ab7c6f0621d401e635d2c79fc442b0ecca
qingshan412 <qingshan412@hotmail.com>
2020-03-20 16:20:24 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,23 @@
 
 module load python
 
-python face_verify_noonan_fold.py > data/facebank/default_2
-python face_verify_noonan_fold.py -tta > data/facebank/tta_1
-python face_verify_noonan_fold.py -s > data/facebank/shuffle_1
-python face_verify_noonan_fold.py -s -tta > data/facebank/shuffle_tta_1
+module load pytorch
+
+for Model in fr_mix_aug_pix2pix_transfer_b6_2000_DG fr_mix_pix2pix_transfer_b6_2000_DG
+do
+    echo "${Model}"
+    python roc_acc_fold.py -d distinct -g 0 -a fake_${Model} > data/facebank/roc_dist_${Model}
+    tail -n 2 data/facebank/roc_dist_${Model}
+    echo "${Model}_s"
+    python roc_acc_fold.py -d distinct -g 0 -s -a fake_${Model} > data/facebank/roc_dist_${Model}_s
+    tail -n 2 data/facebank/roc_dist_${Model}_s
+    echo "${Model}_tta"
+    python roc_acc_fold.py -d distinct -g 0 -tta -a fake_${Model} > data/facebank/roc_dist_${Model}_tta
+    tail -n 2 data/facebank/roc_dist_${Model}_tta
+done
+
+
+# python face_verify_noonan_fold.py > data/facebank/default_2
+# python face_verify_noonan_fold.py -tta > data/facebank/tta_1
+# python face_verify_noonan_fold.py -s > data/facebank/shuffle_1
+# python face_verify_noonan_fold.py -s -tta > data/facebank/shuffle_tta_1
