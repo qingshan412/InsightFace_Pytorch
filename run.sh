51e47c27d6c60df2bba6a5bbd53115f2ec797c4b
qingshan412 <qingshan412@hotmail.com>
2020-03-28 16:42:04 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,18 +10,30 @@ module load python
 
 module load pytorch
 
-for Model in fr_mix_aug_pix2pix_transfer_b6_2000_DG fr_mix_pix2pix_transfer_b6_2000_DG
-do
-    echo "${Model}"
-    python roc_acc_fold.py -d distinct -g 0 -a fake_${Model} > data/facebank/roc_dist_${Model}
-    tail -n 2 data/facebank/roc_dist_${Model}
-    echo "${Model}_s"
-    python roc_acc_fold.py -d distinct -g 0 -s -a fake_${Model} > data/facebank/roc_dist_${Model}_s
-    tail -n 2 data/facebank/roc_dist_${Model}_s
-    echo "${Model}_tta"
-    python roc_acc_fold.py -d distinct -g 0 -tta -a fake_${Model} > data/facebank/roc_dist_${Model}_tta
-    tail -n 2 data/facebank/roc_dist_${Model}_tta
-done
+Model=fr_mix_aug_pix2pix_transfer_b6_100_pool5_full
+echo "${Model}"
+python roc_acc_fold.py -d distinct -g 0 -a fake_${Model} > data/facebank/roc_dist_${Model}
+tail -n 2 data/facebank/roc_dist_${Model}
+echo "${Model}_s"
+python roc_acc_fold.py -d distinct -g 0 -s -a fake_${Model} > data/facebank/roc_dist_${Model}_s
+tail -n 2 data/facebank/roc_dist_${Model}_s
+echo "${Model}_tta"
+python roc_acc_fold.py -d distinct -g 0 -tta -a fake_${Model} > data/facebank/roc_dist_${Model}_tta
+tail -n 2 data/facebank/roc_dist_${Model}_tta
+
+
+# for Model in fr_mix_aug_pix2pix_transfer_b6_2000_DG fr_mix_pix2pix_transfer_b6_2000_DG
+# do
+#     echo "${Model}"
+#     python roc_acc_fold.py -d distinct -g 0 -a fake_${Model} > data/facebank/roc_dist_${Model}
+#     tail -n 2 data/facebank/roc_dist_${Model}
+#     echo "${Model}_s"
+#     python roc_acc_fold.py -d distinct -g 0 -s -a fake_${Model} > data/facebank/roc_dist_${Model}_s
+#     tail -n 2 data/facebank/roc_dist_${Model}_s
+#     echo "${Model}_tta"
+#     python roc_acc_fold.py -d distinct -g 0 -tta -a fake_${Model} > data/facebank/roc_dist_${Model}_tta
+#     tail -n 2 data/facebank/roc_dist_${Model}_tta
+# done
 
 
 # python face_verify_noonan_fold.py > data/facebank/default_2
