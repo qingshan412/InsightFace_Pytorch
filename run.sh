0813f82b5944f7db90af5f885133280b33cdb3cf
qingshan412 <qingshan412@hotmail.com>
2020-04-01 13:55:38 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,18 +10,33 @@ module load python
 
 module load pytorch
 
-for Model in fr_mix_aug_pix2pix_transfer_b6_100_pool5_full fr_mix_aug_pix2pix_transfer_b6_100_full
+python evaluate_model.py
+
+for Model in fr_mix_aug_pix2pix_transfer_b6_500_pool5_full fr_mix_aug_pix2pix_transfer_b6_500_full
 do
     echo "${Model}"
-    python roc_acc_fold.py -d divided -g 0 -a fake_${Model} > data/facebank/roc_divi_${Model}
-    tail -n 2 data/facebank/roc_divi_${Model}
+    python roc_acc_fold.py -d distinct -g 0 -a fake_${Model} > data/facebank/roc_dist_${Model}
+    tail -n 2 data/facebank/roc_dist_${Model}
     echo "${Model}_s"
-    python roc_acc_fold.py -d divided -g 0 -s -a fake_${Model} > data/facebank/roc_divi_${Model}_s
-    tail -n 2 data/facebank/roc_divi_${Model}_s
+    python roc_acc_fold.py -d distinct -g 0 -s -a fake_${Model} > data/facebank/roc_dist_${Model}_s
+    tail -n 2 data/facebank/roc_dist_${Model}_s
     echo "${Model}_tta"
-    python roc_acc_fold.py -d divided -g 0 -tta -a fake_${Model} > data/facebank/roc_divi_${Model}_tta
-    tail -n 2 data/facebank/roc_divi_${Model}_tta
+    python roc_acc_fold.py -d distinct -g 0 -tta -a fake_${Model} > data/facebank/roc_dist_${Model}_tta
+    tail -n 2 data/facebank/roc_dist_${Model}_tta
 done
+
+# for Model in fr_mix_aug_pix2pix_transfer_b6_100_pool5_full fr_mix_aug_pix2pix_transfer_b6_100_full
+# do
+#     echo "${Model}"
+#     python roc_acc_fold.py -d divided -g 0 -a fake_${Model} > data/facebank/roc_divi_${Model}
+#     tail -n 2 data/facebank/roc_divi_${Model}
+#     echo "${Model}_s"
+#     python roc_acc_fold.py -d divided -g 0 -s -a fake_${Model} > data/facebank/roc_divi_${Model}_s
+#     tail -n 2 data/facebank/roc_divi_${Model}_s
+#     echo "${Model}_tta"
+#     python roc_acc_fold.py -d divided -g 0 -tta -a fake_${Model} > data/facebank/roc_divi_${Model}_tta
+#     tail -n 2 data/facebank/roc_divi_${Model}_tta
+# done
 
 # Model=fr_mix_aug_pix2pix_transfer_b6_100_pool5_full
 # echo "${Model}"
