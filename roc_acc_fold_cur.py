fc4c087a4f222bdd5105035023254dbcfc519b1f
qingshan412 <qingshan412@hotmail.com>
2020-04-04 17:09:34 -0400

    Update roc_acc_fold_cur.py

--- roc_acc_fold_cur.py
+++ roc_acc_fold_cur.py
@@ -230,7 +230,7 @@ if __name__ == '__main__':
     colors = list(mcolors.TABLEAU_COLORS)
     lw = 2
     plt.plot(fpr, tpr, color='darkorange',
-            lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
+            lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
     plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
