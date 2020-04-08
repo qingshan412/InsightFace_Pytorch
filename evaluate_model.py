733edc8980c6e59c7bd3fa4b075beea8bbfda052
qingshan412 <qingshan412@hotmail.com>
2020-04-08 15:31:23 -0400

    Update evaluate_model.py

--- evaluate_model.py
+++ evaluate_model.py
@@ -151,11 +151,13 @@
 ##############################################################################################
 # generate roc/pr images
 ##############################################################################################
-import argparse
+# import argparse
 from data.data_pipe import merge_plt
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='for face verification')
-    parser.add_argument("-n", "--exp_name", help="experiment name", default="", type=str)
-    args = parser.parse_args()
-    merge_plt(args.exp_name)
+merge_plt()
+
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser(description='for face verification')
+#     parser.add_argument("-n", "--exp_name", help="experiment name", default="", type=str)
+#     args = parser.parse_args()
+#     merge_plt(args.exp_name)
