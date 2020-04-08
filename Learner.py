ada9cb63007fce11650d6d343ef79d4a897a7a78
qingshan412 <qingshan412@hotmail.com>
2020-04-08 16:12:32 -0400

    Update Learner.py

--- Learner.py
+++ Learner.py
@@ -1,4 +1,4 @@
-from data.data_pipe import de_preprocess, get_train_loader, get_val_data
+from data.data_pipe import de_preprocess, get_train_loader, get_val_data, get_val_pair
 from model import Backbone, Arcface, MobileFaceNet, Am_softmax, l2_norm
 from verifacation import evaluate
 import torch
