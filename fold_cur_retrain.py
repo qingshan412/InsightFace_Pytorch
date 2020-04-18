6bd78a11e454293f14bf4091f1b86ffd649fce01
qingshan412 <qingshan412@hotmail.com>
2020-04-18 15:52:22 -0400

    Update fold_cur_retrain.py

--- fold_cur_retrain.py
+++ fold_cur_retrain.py
@@ -54,7 +54,11 @@ if __name__ == '__main__':
     if args.tta:
         exp_name += '_tta'
     
-    
+    if args.stylegan_data_dir:
+        #e.g. smile_refine_mtcnn_112_divi
+        full_stylegan_dir = str(conf.data_path/'facebank'/'stylegan'/args.stylegan_data_dir)
+        stylegan_folders = os.listdir(full_stylegan_dir)
+
     # prepare folders
     raw_dir = 'raw_112'
     verify_type = 'verify'
