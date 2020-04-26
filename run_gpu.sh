e65f0f72103fcb55bbdd6c743dceec045815415b
qingshan412 <qingshan412@hotmail.com>
2020-04-26 17:25:06 -0400

    Update run_gpu.sh

--- run_gpu.sh
+++ run_gpu.sh
@@ -9,29 +9,46 @@
 
 module load pytorch
 
-DataDir=divided
+DataDir=ref_noonan
 LagData=LAG_y_fine
 Model=smile_refine_mtcnn_112_divi
-Op=train
-echo ${Op}
-python fold_cur.py -d ${DataDir} -g 0 -a ${LagData} -as ${Model} -ts ${Op} \
-> data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_styl_${Op}
-python fold_cur.py -d ${DataDir} -g 0 -s -a ${LagData} -as ${Model} -ts ${Op} \
-> data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_styl_${Op}_s
-python fold_cur.py -d ${DataDir} -g 0 -tta -a ${LagData} -as ${Model} -ts ${Op} \
-> data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_styl_${Op}_tta
-python fold_cur.py -d ${DataDir} -g 0 -a ${LagData} -ts ${Op} \
-> data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_${Op}
-python fold_cur.py -d ${DataDir} -g 0 -s -a ${LagData} -ts ${Op} \
-> data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_${Op}_s
-python fold_cur.py -d ${DataDir} -g 0 -tta -a ${LagData} -ts ${Op} \
-> data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_${Op}_tta
-python fold_cur.py -d ${DataDir} -g 0 -as ${Model} -ts ${Op} \
-> data/facebank/trans/plt_recs/no_trans_${DataDir}_styl_${Op}
-python fold_cur.py -d ${DataDir} -g 0 -s -as ${Model} -ts ${Op} \
-> data/facebank/trans/plt_recs/no_trans_${DataDir}_styl_${Op}_s
-python fold_cur.py -d ${DataDir} -g 0 -tta -as ${Model} -ts ${Op} \
-> data/facebank/trans/plt_recs/no_trans_${DataDir}_styl_${Op}_tta
+python fold_cur.py -d ${DataDir} -g 0 \
+> data/facebank/trans/plt_recs/no_trans_${DataDir}
+python fold_cur.py -d ${DataDir} -g 0 -s \
+> data/facebank/trans/plt_recs/no_trans_${DataDir}_s
+python fold_cur.py -d ${DataDir} -g 0 -tta \
+> data/facebank/trans/plt_recs/no_trans_${DataDir}_tta
+# for Op in "train" "test" "train,test"
+# do
+# python fold_cur.py -d ${DataDir} -g 0 -a ${LagData} -as ${Model} -ts ${Op} \
+# > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_styl_${Op}
+# python fold_cur.py -d ${DataDir} -g 0 -s -a ${LagData} -as ${Model} -ts ${Op} \
+# > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_styl_${Op}_s
+# python fold_cur.py -d ${DataDir} -g 0 -tta -a ${LagData} -as ${Model} -ts ${Op} \
+# > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_styl_${Op}_tta
+# done
+
+# DataDir=divided
+# Op=train
+# echo ${Op}
+# python fold_cur.py -d ${DataDir} -g 0 -a ${LagData} -as ${Model} -ts ${Op} \
+# > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_styl_${Op}
+# python fold_cur.py -d ${DataDir} -g 0 -s -a ${LagData} -as ${Model} -ts ${Op} \
+# > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_styl_${Op}_s
+# python fold_cur.py -d ${DataDir} -g 0 -tta -a ${LagData} -as ${Model} -ts ${Op} \
+# > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_styl_${Op}_tta
+# python fold_cur.py -d ${DataDir} -g 0 -a ${LagData} -ts ${Op} \
+# > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_${Op}
+# python fold_cur.py -d ${DataDir} -g 0 -s -a ${LagData} -ts ${Op} \
+# > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_${Op}_s
+# python fold_cur.py -d ${DataDir} -g 0 -tta -a ${LagData} -ts ${Op} \
+# > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_${Op}_tta
+# python fold_cur.py -d ${DataDir} -g 0 -as ${Model} -ts ${Op} \
+# > data/facebank/trans/plt_recs/no_trans_${DataDir}_styl_${Op}
+# python fold_cur.py -d ${DataDir} -g 0 -s -as ${Model} -ts ${Op} \
+# > data/facebank/trans/plt_recs/no_trans_${DataDir}_styl_${Op}_s
+# python fold_cur.py -d ${DataDir} -g 0 -tta -as ${Model} -ts ${Op} \
+# > data/facebank/trans/plt_recs/no_trans_${DataDir}_styl_${Op}_tta
 
 # echo ${DataDir}
 # for Op in "test" "train,test"
