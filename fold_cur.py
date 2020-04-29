e9bdd9b41f2ef298e43cefd7eb172a599eeb9722
qingshan412 <qingshan412@hotmail.com>
2020-04-29 12:36:05 -0400

    Update fold_cur.py

--- fold_cur.py
+++ fold_cur.py
@@ -38,8 +38,8 @@ if __name__ == '__main__':
                         default="", type=str)
     args = parser.parse_args()
 
-    print(args.dataset_dir)
-    exit(0)
+    for arg in vars(args):
+        print(arg+':', getattr(args, arg))
 
     conf = get_config(False, args)
 
