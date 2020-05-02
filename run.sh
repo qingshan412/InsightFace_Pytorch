73bfe74f67889aad4a0a34b564afff82afc83154
qingshan412 <qingshan412@hotmail.com>
2020-05-02 15:03:00 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,49 +11,75 @@ module load python pytorch
 
 DataDir=divided
 LagData=LAG_y_fine
-Model=srm112df_nn
 
-for Op in "train" "test" "train,test"
+for Model in srm112df_nn inn05_112for 
 do
-    python fold_cur.py -ds ${DataDir} -g 0 \
-    -as ${Model} -ts ${Op} \
-    > data/facebank/trans/plt_recs/no_trans_${DataDir}_${Model}_${Op}
+    for Op in "train" "train,test"
+    do
+        python fold_cur.py -ds ${DataDir} -g 0 \
+        -a ${LagData} -ta ${Op} \
+        -as ${Model} -ts ${Op} \
+        > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_test_${Model}_${Op}
 
-    python fold_cur.py -ds ${DataDir} -g 0 -s \
-    -as ${Model} -ts ${Op} \
-    > data/facebank/trans/plt_recs/no_trans_${DataDir}_${Model}_${Op}_s
+        python fold_cur.py -ds ${DataDir} -g 0 -s \
+        -a ${LagData} -ta ${Op} \
+        -as ${Model} -ts ${Op} \
+        > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_test_${Model}_${Op}_s
 
-    python fold_cur.py -ds ${DataDir} -g 0 -s -rs 888\
-    -as ${Model} -ts ${Op} \
-    > data/facebank/trans/plt_recs/no_trans_${DataDir}_${Model}_${Op}_s888
+        python fold_cur.py -ds ${DataDir} -g 0 -s -rs 888\
+        -a ${LagData} -ta ${Op} \
+        -as ${Model} -ts ${Op} \
+        > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_test_${Model}_${Op}_s888
 
-    python fold_cur.py -ds ${DataDir} -g 0 -tta \
-    -as ${Model} -ts ${Op} \
-    > data/facebank/trans/plt_recs/no_trans_${DataDir}_${Model}_${Op}_tta
+        python fold_cur.py -ds ${DataDir} -g 0 -tta \
+        -a ${LagData} -ta ${Op} \
+        -as ${Model} -ts ${Op} \
+        > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_test_${Model}_${Op}_tta
+    done
 done
 
-for Op in "test" "train,test"
-do
-    python fold_cur.py -ds ${DataDir} -g 0 \
-    -a ${LagData} -ta "test" \
-    -as ${Model} -ts ${Op} \
-    > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_test_${Model}_${Op}
+# Model=srm112df_nn
+# for Op in "train" "test" "train,test"
+# do
+#     python fold_cur.py -ds ${DataDir} -g 0 \
+#     -as ${Model} -ts ${Op} \
+#     > data/facebank/trans/plt_recs/no_trans_${DataDir}_${Model}_${Op}
 
-    python fold_cur.py -ds ${DataDir} -g 0 -s \
-    -a ${LagData} -ta "test" \
-    -as ${Model} -ts ${Op} \
-    > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_test_${Model}_${Op}_s
+#     python fold_cur.py -ds ${DataDir} -g 0 -s \
+#     -as ${Model} -ts ${Op} \
+#     > data/facebank/trans/plt_recs/no_trans_${DataDir}_${Model}_${Op}_s
 
-    python fold_cur.py -ds ${DataDir} -g 0 -s -rs 888\
-    -a ${LagData} -ta "test" \
-    -as ${Model} -ts ${Op} \
-    > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_test_${Model}_${Op}_s888
+#     python fold_cur.py -ds ${DataDir} -g 0 -s -rs 888\
+#     -as ${Model} -ts ${Op} \
+#     > data/facebank/trans/plt_recs/no_trans_${DataDir}_${Model}_${Op}_s888
 
-    python fold_cur.py -ds ${DataDir} -g 0 -tta \
-    -a ${LagData} -ta "test" \
-    -as ${Model} -ts ${Op} \
-    > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_test_${Model}_${Op}_tta
-done
+#     python fold_cur.py -ds ${DataDir} -g 0 -tta \
+#     -as ${Model} -ts ${Op} \
+#     > data/facebank/trans/plt_recs/no_trans_${DataDir}_${Model}_${Op}_tta
+# done
+
+# for Op in "test" "train,test"
+# do
+#     python fold_cur.py -ds ${DataDir} -g 0 \
+#     -a ${LagData} -ta "test" \
+#     -as ${Model} -ts ${Op} \
+#     > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_test_${Model}_${Op}
+
+#     python fold_cur.py -ds ${DataDir} -g 0 -s \
+#     -a ${LagData} -ta "test" \
+#     -as ${Model} -ts ${Op} \
+#     > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_test_${Model}_${Op}_s
+
+#     python fold_cur.py -ds ${DataDir} -g 0 -s -rs 888\
+#     -a ${LagData} -ta "test" \
+#     -as ${Model} -ts ${Op} \
+#     > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_test_${Model}_${Op}_s888
+
+#     python fold_cur.py -ds ${DataDir} -g 0 -tta \
+#     -a ${LagData} -ta "test" \
+#     -as ${Model} -ts ${Op} \
+#     > data/facebank/trans/plt_recs/no_trans_${DataDir}_lag_test_${Model}_${Op}_tta
+# done
 
 # Model=inn05_112
 
